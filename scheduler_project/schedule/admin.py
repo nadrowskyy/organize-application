ea1c04329001bf1f2331982d1bd88d96703d7861
Tomasz <nadrowskia2@gmail.com>
2021-03-21 22:15:17 +0100

    Added model Event do admin site and refine Event model

--- scheduler_project/schedule/admin.py
+++ scheduler_project/schedule/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Event
 
-# Register your models here.
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'organizer', 'planning_date')
+    list_filter = ('title', 'created', 'publish', 'organizer')
+    search_fields = ('title', 'description')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('organizer',)
+    date_hierarchy = 'publish'
+    ordering = ('status', 'publish')
--- scheduler_project/schedule/models.py
+++ scheduler_project/schedule/models.py
@@ -6,21 +6,21 @@ from django.utils import timezone
 class Event(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Szkic'),
-        ('published', 'Opublikowano')
+        ('publish', 'Opublikowano')
     )
     title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=250,
+                            unique_for_date='planning_date', default=None)
     description = models.TextField(verbose_name='opis wydarzenia')
     created = models.DateTimeField(auto_now_add=True)
     planning_date = models.DateTimeField()
     publish = models.DateTimeField(default=timezone.now)
-    # participants
     organizer = models.ForeignKey(User, on_delete=models.CASCADE)
-    # topics
     status = models.CharField(max_length=15,
                               choices=STATUS_CHOICES,
                               default='draft')
-    # comments
     duration = models.IntegerField()
+    # topics
 
     class Meta:
         ordering = ('-publish',)
