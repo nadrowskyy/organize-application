3bf815c513490aa3cb3a9c89f128453f5e46d381
Bartek Kustra <polishgunner13@gmail.com>
2021-03-22 20:33:29 +0100

    Test kom Bartka K

--- scheduler_project/schedule/models.py
+++ scheduler_project/schedule/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
+#komentarz test Bartek K
 
 class Event(models.Model):
     STATUS_CHOICES = (
