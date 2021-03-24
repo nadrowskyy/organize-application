b3e6aa167ec16c1f21e06fc6ab0d4edaa922aa6a
Tomasz <nadrowskia2@gmail.com>
2021-03-24 15:46:52 +0100

    Login form init commit

--- scheduler_project/schedule/views.py
+++ scheduler_project/schedule/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+def login_page(request):
+    context = {}
+    return render(request, 'schedule/login.html')
+
+
+def register_page(request):
+    context = {}
+    return render(request, 'schedule/register.html')
+
--- scheduler_project/scheduler_project/urls.py
+++ scheduler_project/scheduler_project/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from schedule.views import register_page, login_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', register_page),
+    path('login/', login_page),
 ]
