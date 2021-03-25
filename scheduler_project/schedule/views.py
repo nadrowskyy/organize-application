f86b72b6d1743e42cdd4fbc2288dca6178b578b4
Tomasz <nadrowskia2@gmail.com>
2021-03-25 11:22:57 +0100

    Commit for login and registration pages

--- scheduler_project/schedule/views.py
+++ scheduler_project/schedule/views.py
@@ -1,13 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+
+
+def home_page(request):
+    return render(request, 'schedule/home.html')
 
 
 def login_page(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')  # html name="username"
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+
     context = {}
     return render(request, 'schedule/login.html')
 
 
 def register_page(request):
-    context = {}
-    return render(request, 'schedule/register.html')
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, f'Account was created for {user}')
+
+            return redirect('login')
+
+    context = {'form': form}
+    return render(request, 'schedule/register.html', context)
 
--- scheduler_project/scheduler_project/urls.py
+++ scheduler_project/scheduler_project/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from schedule.views import register_page, login_page
+from schedule.views import register_page, login_page, home_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_page, name='home'),
     path('register/', register_page),
-    path('login/', login_page),
+    path('login/', login_page, name='login'),
 ]
