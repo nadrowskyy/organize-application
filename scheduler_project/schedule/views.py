3f78d6d78035c3ec99f0079df3a373614943be09
Bartek Kustra <polishgunner13@gmail.com>
2021-03-27 00:09:08 +0100

    dodana lista eventów, posortowana datą

--- scheduler_project/schedule/views.py
+++ scheduler_project/schedule/views.py
@@ -6,6 +6,21 @@ from django.contrib.auth import authenticate, login, logout
 import calendar
 from calendar import HTMLCalendar
 from datetime import datetime
+from django.contrib.auth.models import Group
+from django.contrib.auth.decorators import login_required
+from .decorators import unauthenticated_user, allowed_users
+from django.http import HttpResponseRedirect
+from .models import Event
+
+# from django.shortcuts import render, redirect
+# from django.contrib.auth.forms import UserCreationForm
+# from .forms import CreateUserForm
+# from django.contrib import messages
+# from django.contrib.auth import authenticate, login, logout
+# import calendar
+# from calendar import HTMLCalendar
+# from datetime import datetime
+# from .models import Event
 
 def home_page(request):
     return render(request, 'schedule/home.html', {})
@@ -52,6 +67,8 @@ def events_list(request, year=datetime.now().year, month=datetime.now().strftime
     present_month = present.month
     present_time = present.strftime('%H:%M:%S')
 
+    all_events_list = Event.objects.all()
+
     return render(request, 'schedule/events_list.html',
                   {
                       "year": year,
@@ -61,4 +78,25 @@ def events_list(request, year=datetime.now().year, month=datetime.now().strftime
                       "present_year": present_year,
                       "present_month": present_month,
                       "present_time": present_time,
+                      'all_events_list': all_events_list,
                   })
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
+
+@allowed_users(allowed_roles=['admin'])
+def create_event(request):
+
+    if request.method == 'POST':
+        subject = request.POST.get('subject')
+        print(subject)
+        description = request.POST.get('description')
+        print(description)
+        organizer = request.POST.get('organizer')
+        start_date = request.POST.get('startdate')
+        print(start_date)
+        duration = request.POST.get('duration')
+        print(duration)
+
+    return render(request, 'schedule/create_event.html')
