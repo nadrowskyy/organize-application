450a5c2fc88d33f0bb70b46243c291d6abd0ab81
Bartek Kustra <polishgunner13@gmail.com>
2021-03-26 13:47:29 +0100

    Strona główna, navbar i wejście na stronę events_list.html (na niej kalendarz)

--- scheduler_project/schedule/views.py
+++ scheduler_project/schedule/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
-
-# Create your views here.
-
+import calendar
+from calendar import HTMLCalendar
+from datetime import datetime
 
 def home(request):
-    return render(request, 'home.html', {})
+    return render(request, 'schedule/home.html', {})
+
+def events_list(request, year=datetime.now().year, month=datetime.now().strftime('%B')):
+    month = month.capitalize()
+    month_number = list(calendar.month_name).index(month)
+    month_number = int(month_number)
+
+    cal = HTMLCalendar().formatmonth(year, month_number)
+
+    present = datetime.now()
+    present_year = present.year
+    present_month = present.month
+    present_time = present.strftime('%H:%M:%S')
+
+    return render(request, 'schedule/events_list.html',
+                  {
+                      "year": year,
+                      "month": month,
+                      "month_number": month_number,
+                      "cal": cal,
+                      "present_year": present_year,
+                      "present_month": present_month,
+                      "present_time": present_time,
+                  })
