5e86ee897f4e8d6a2369b434511e75f92d16ecdb
mrudawski <m.rudawski777@gmail.com>
2021-03-31 18:25:26 +0200

    Zmieniony homepage - karuzela z instrukcją, wyświetlanie trzech najbliższch eventów z datą, godziną, imieniem i nazwiskiem; miejsce na kolejne dodatki

--- scheduler_project/schedule/models.py
+++ scheduler_project/schedule/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from datetime import date
 
 
 class Event(models.Model):
@@ -27,3 +28,8 @@ class Event(models.Model):
 
     def __str__(self):
         return self.title
+
+
+@property
+def is_past_due(self):
+    return date.today() > self.date
