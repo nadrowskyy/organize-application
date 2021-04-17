0f656f908aab290eb16e4d12756b1ecf173b5085
Tomasz <nadrowskia2@gmail.com>
2021-04-18 00:27:26 +0200

    Groups 'admin' and 'employee' are added automaticalyy when creating migrations to database. Superuser is created with login: 'superuser', password: 'super'

--- scheduler_project/schedule/apps.py
+++ scheduler_project/schedule/apps.py
@@ -1,5 +1,11 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+from django.contrib.auth.models import Group
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+from .models import populate_models
 
 
 class ScheduleConfig(AppConfig):
     name = 'schedule'
+    verbose_name = 'schedule'
