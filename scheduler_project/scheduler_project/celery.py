e421ae0825109f385b45b2ad22c061891c851f19
mrudawski <m.rudawski777@gmail.com>
2021-06-09 17:19:44 +0200

    dockerizing celery...

--- scheduler_project/scheduler_project/celery.py
+++ scheduler_project/scheduler_project/celery.py
@@ -6,7 +6,7 @@ from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scheduler_project.settings')
 
-app = Celery('scheduler_project')
+app = Celery('scheduler_project', broker="amqp://rabbitmq")
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.conf.timezone = 'Europe/Warsaw'
