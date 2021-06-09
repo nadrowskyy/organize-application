cccc28c4284e5228221e3d0b81f6778562e4cdfd
mrudawski <73662845+mrudawski@users.noreply.github.com>
2021-06-09 14:05:38 +0200

    Update celery.py

--- scheduler_project/scheduler_project/celery.py
+++ scheduler_project/scheduler_project/celery.py
@@ -1,8 +1,11 @@
 from __future__ import absolute_import, unicode_literals
 import os
+import dotenv
 from celery import Celery
 from django.conf import settings
 from celery.schedules import crontab
+
+dotenv.read_dotenv()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scheduler_project.settings')
 
